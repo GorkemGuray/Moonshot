c039bdc59e7ee16cbd381068ace1544c09dcf887
Görkem Güray <grkmgry@gmail.com>
2024-04-14 12:53:12 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Görkem Güray on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

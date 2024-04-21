d0b8bf39b8209f7e61209dd499b0880218f2e738
Görkem Güray <grkmgry@gmail.com>
2024-04-21 13:22:16 +0300

    part-1 correction

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,17 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CustomText: View {
+    let text: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
+
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        let astronauts = Bundle.main.decode("astronauts.json")
+        Button("Test") {
+            dump(astronauts)
+        }
+        Text(String(astronauts.count))
+
+    }//body
+}//ContentView
 
 #Preview {
     ContentView()
